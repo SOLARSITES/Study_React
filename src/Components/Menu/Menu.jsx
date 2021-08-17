ae35a7bb30afc2a5237e64e7f6db338bf9e0b33b
SOLARSITES <iourion@gmail.com>
2021-08-17 16:09:09 +0300

    added: lesson14 done

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import dbMenu from '../DBMenu';
 import { ListItem } from './ListItem';
 import { Banner } from './Banner';
+import { useFetch } from '../Hooks/useFetch';
+import loadImg from '../../image/load.svg';
+import errorImg from '../../image/error.png';
 
 const MenuStyled = styled.main`
   background-color: #ccc;
@@ -14,16 +16,38 @@ const SectionMenu = styled.section`
   padding: 30px;
 `;
 
-export const Menu = ({ setOpenItem }) => (
-  <MenuStyled>
-    <Banner />
-    <SectionMenu>
-      <h2>Бургеры</h2>
-      <ListItem itemList={dbMenu.burger} setOpenItem={setOpenItem} />
-    </SectionMenu>
-    <SectionMenu>
-      <h2>Закуски / Напитки</h2>
-      <ListItem itemList={dbMenu.other} setOpenItem={setOpenItem} />
-    </SectionMenu>
-  </MenuStyled>
-);
+const ImageWrap = styled.div`
+  text-align: center;
+  padding: 25px 25px 250px 25px;
+`;
+
+export const Menu = ({ setOpenItem }) => {
+  const res = useFetch();
+  const dbMenu = res.response;
+
+  return (
+    <MenuStyled>
+      <Banner />
+      {res.response ? (
+        <>
+          <SectionMenu>
+            <h2>Бургеры</h2>
+            <ListItem itemList={dbMenu.burger} setOpenItem={setOpenItem} />
+          </SectionMenu>
+          <SectionMenu>
+            <h2>Закуски / Напитки</h2>
+            <ListItem itemList={dbMenu.other} setOpenItem={setOpenItem} />
+          </SectionMenu>
+        </>
+      ) : res.error ? (
+        <ImageWrap>
+          <img src={errorImg} alt="Ошибка загрузки данных!" />
+        </ImageWrap>
+      ) : (
+        <ImageWrap>
+          <img src={loadImg} alt="Загрузка данных..." />
+        </ImageWrap>
+      )}
+    </MenuStyled>
+  );
+};
